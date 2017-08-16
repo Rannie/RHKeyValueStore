fcc55749fecb482a04dd97a318404bf5ae3b41b8
Hanran Liu <18622218653@163.com>
2017-08-16 16:21:47 +0800

    demo init

---
+++ KVStoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVStoreDemo
+//
+//  Created by ran on 2017/8/16.
+//  Copyright © 2017年 ran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
